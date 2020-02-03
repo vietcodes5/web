bbf2924d201bce1876b05da21ed479b8c6e126e2
mactungmindx <tungmt@mindxtech.com>
2020-02-03 11:08:59 +0700

    add click event detail

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import About from '../../pages/about.js'
 import Home from '../../pages/home.js'
 import Events from '../../pages/events.js'
+import EventDetail from '../../templates/EventDetail'
 import Posts from '../../pages/posts.js'
 import EditPost from '../../pages/admin/edit_post.js'
 import Login from '../../pages/admin/login.js'
@@ -11,12 +12,12 @@ import Login from '../../pages/admin/login.js'
 export default function Content(props) {
     return (
         <div className="container" style={{
-            minHeight: "100vh",
+            minHeight: "90vh",
             marginTop: "80px"}}>
             <Switch>
                 <Route path="/about" component={About} />
                 <Route path="/posts" component={Posts} />
-                <Route path="/events/:name" component={Event} />
+                <Route path="/events/:id" component={EventDetail} />
                 <Route path="/events" component={Events} />
                 <Route path="/edit-post" component={EditPost} />
                 <Route path="/admin/login" component={Login} />
--- src/templates/Event.js
+++ src/templates/Event.js
@@ -1,23 +1,22 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-export default class Event extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {name: props.name};
-    }
+let backendUrl = 'http://localhost:8080';
 
-    render() {
-        let src = `http://localhost:8080/events/${this.state.name}/photo/main/main-square.jpg`;
-        return (
-            <div className="card my-3" style={{width: "18rem"}}>
-                <img src={src} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title">{this.state.name}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <Link to="/" className="btn btn-info">Go somewhere</Link>
-                </div>
+export default function Event(props) {
+    
+    let thumbnailImgSrc = `${backendUrl}/public/photos/${props.main_photos.square}`;
+
+    
+
+    return (
+        <div className="card my-3" style={{width: "18rem"}}>
+            <img src={thumbnailImgSrc} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
+                <p className="card-text">{props.subTitle}</p>
+                <Link to={`events/${props.id}`} className="btn btn-info">Enter Event</Link>
             </div>
-        )
-    }
+        </div>
+    )
 }
