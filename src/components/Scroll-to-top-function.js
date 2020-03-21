0ad768c5dc542bc6430e9809355a6b8d4d866bd8
MyNameIsHung <46376260+MyNameIsHung@users.noreply.github.com>
2020-03-21 12:08:25 +0700

    Update Scroll-to-top-function.js
    Make scroll-to behavior go smoothly

--- src/components/Scroll-to-top-function.js
+++ src/components/Scroll-to-top-function.js
@@ -4,7 +4,10 @@ import { withRouter } from "react-router-dom";
  class ScrollToTop extends Component {
      componentDidUpdate(prevProps) {
          if (this.props.location !== prevProps.location) {
-             window.scrollTo(0, 0);
+             window.scrollTo({
+                 top: '0',
+                 behavior: 'smooth',
+             });
          }
      }
 
