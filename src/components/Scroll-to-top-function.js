88235789964c4e84e68862cd030fad8e1c8b918a
MyNameIsHung <46376260+MyNameIsHung@users.noreply.github.com>
2020-03-22 08:24:20 +0700

    Update Scroll-to-top-function.js
    Updated ScrollToTop

--- src/components/Scroll-to-top-function.js
+++ src/components/Scroll-to-top-function.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 
- class ScrollToTop extends Component {
-     componentDidUpdate(prevProps) {
-         if (this.props.location !== prevProps.location) {
-             window.scrollTo({
-                 top: '0',
-                 behavior: 'smooth',
-             });
-         }
-     }
 
-     render() {
-         return this.props.children
-     }
- }
+function ScrollToTop(props) {
+  const prevProps = useRef();
+  useEffect(() => {
+    if(props.location !== prevProps.location) {
+      window.scrollTo({
+        top: '0',
+        behavior: 'smooth',
+      });
+    }
+  });
+    return props.children;
+}
 
- export default withRouter(ScrollToTop);
+export default withRouter(ScrollToTop);
