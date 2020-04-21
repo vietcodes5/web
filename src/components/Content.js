f29af1c28953f298c0bd67484bddb8204238152c
MyNameIsHung <46376260+MyNameIsHung@users.noreply.github.com>
2020-04-21 09:49:09 +0700

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,7 +8,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
   marginApp: {
     paddingTop: "60px",
-    overflow: 'hidden'
+    overflow: 'hidden',
+    minHeight: '80vh',
   },
 }));
 
